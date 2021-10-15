e2058a971192c02767f63ccb5743d77ec85d4e1e
FaabsD <fabian.et.je@hotmail.com>
2021-10-15 13:11:53 +0200

    created first version of API route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// API route
+Route::get('/getStedentripsAPI', [getStedentripsApi::class])->name('stedentripsAPI');
