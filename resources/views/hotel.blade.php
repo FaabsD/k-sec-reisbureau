cb521650a8d2227421bd7a46c4742c1acb25f37f
FaabsD <fabian.et.je@hotmail.com>
2021-10-30 11:20:23 +0200

    added a little styling

--- resources/views/hotel.blade.php
+++ resources/views/hotel.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.main')
+@section('page_title')
+    Hotel inter
+@endsection
 @section('header')
     <header class="w-full">
         <div>
@@ -11,12 +14,16 @@
 @endsection
 
 @section('content')
-    <div>
-        @foreach($data['hotelRooms'] as $room)
-            <div>
+    <div class="grid grid-cols-3 gap-x-4 container mx-auto">
+        @foreach( $data['hotelRooms'] as $room )
+            <div class="px-4 py-2 rounded bg-gray-100">
                 <h2>{{ $room['room_name'] }}</h2>
                 <p>
-                    {{ $room['location'] }}
+                    {{ $room['location'] }} <br>
+                    {{ $room['price'] }} <br>
+                </p>
+                <p>
+                    {{ $room['details'] }}
                 </p>
             </div>
         @endforeach
