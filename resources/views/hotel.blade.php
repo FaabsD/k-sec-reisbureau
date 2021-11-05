f119ef712cda4252183dc77d116f8559fb018bd6
FaabsD <fabian.et.je@hotmail.com>
2021-11-05 10:53:44 +0100

    replaced layout for app layout added by jetstream

--- resources/views/hotel.blade.php
+++ resources/views/hotel.blade.php
@@ -1,8 +1,8 @@
-@extends('layouts.main')
+<x-app-layout>
 @section('page_title')
     Hotel inter
 @endsection
-@section('header')
+<x-slot name="header">
     <header class="w-full">
         <div>
             <h2 class="text-2xl">
@@ -11,9 +11,8 @@
         </div>
 
     </header>
-@endsection
-
-@section('content')
+</x-slot>
+<x-slot name="slot">
     <div class="grid grid-cols-3 gap-x-4 container mx-auto">
         @foreach( $data['hotelRooms'] as $room )
             <div class="px-4 py-2 rounded bg-gray-100">
@@ -28,4 +27,5 @@
             </div>
         @endforeach
     </div>
-@endsection
+</x-slot>
+</x-app-layout>
