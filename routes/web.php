6dd808abe9304de56c13484273ca52b4c81ef8c9
FaabsD <fabian.et.je@hotmail.com>
2022-01-13 10:51:29 +0100

    added auth middleware to getStadentrips route

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::prefix('api')->group(function () {
         ->name('stedentripsAPI');
 });
 
-Route::get('/getStedentrips', [getStedentrips::class, 'index'])->name('stedentrips');
+Route::middleware('auth:sanctum')->get('/getStedentrips', [getStedentrips::class, 'index'])->name('stedentrips');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
